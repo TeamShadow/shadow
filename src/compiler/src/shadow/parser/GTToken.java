399eb2da4722bf1f24a86b1730a47c65d46be698
bwittman <bwittman@d82a048f-d6e0-442c-bf07-03e2631dfcd4>
2009-11-24 06:14:34 +0000

    Added custom GTToken productions to Token.

--- src/compiler/src/shadow/parser/GTToken.java
+++ src/compiler/src/shadow/parser/GTToken.java
@@ -3,6 +3,15 @@ package shadow.parser;
 import shadow.parser.javacc.ShadowParserConstants;
 import shadow.parser.javacc.Token;
 
-public class GTToken extends Token {
-	public int realKind = ShadowParserConstants.GT;
+public final class GTToken extends Token {
+    public int realKind = ShadowParserConstants.GT;
+    public GTToken(int ofKind, String image) {
+       super(ofKind, image);
+    }
+
+ public static Token newToken(int ofKind)
+ {
+    return newToken(ofKind, null);
+ }
+
 }
