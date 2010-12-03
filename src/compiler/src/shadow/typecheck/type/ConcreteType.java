238cb67a064ed5e8da301b6684f07a8ab6692158
bwittman <bwittman@d82a048f-d6e0-442c-bf07-03e2631dfcd4>
2010-12-03 23:28:25 +0000

    Added a test that shows our inability to correctly detect lvalue vs. rvalue issues.

---
+++ src/compiler/src/shadow/typecheck/type/ConcreteType.java
@@ -0,0 +1,17 @@
+package shadow.typecheck.type;
+
+public class ConcreteType
+{
+	private Type type;
+	private int attributes;
+	
+	//final
+	//lvalue
+	
+	
+	public ConcreteType( Type type, int attributes )
+	{
+		
+		
+	}
+}
