238f51bd10d10e5b4db1940a55f8ea535dc6f377
wspeirs <wspeirs@d82a048f-d6e0-442c-bf07-03e2631dfcd4>
2011-02-05 19:03:05 +0000

    - Getting it so you can just get the output of C code

--- src/compiler/src/shadow/Main.java
+++ src/compiler/src/shadow/Main.java
@@ -3,12 +3,19 @@
  */
 package shadow;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import shadow.TAC.TACBuilder;
+import shadow.TAC.TACMethod;
+import shadow.output.TACLinearWalker;
+import shadow.output.C.TACCVisitor;
 import shadow.parser.javacc.ParseException;
+import shadow.parser.javacc.ShadowException;
 import shadow.parser.javacc.ShadowParser;
 import shadow.parser.javacc.SimpleNode;
+import shadow.typecheck.TypeChecker;
 
 /**
  * @author wspeirs
@@ -23,20 +30,45 @@ public class Main {
 	public static void main(String[] args) {
 		
 		try {
-			FileInputStream sourceStream = new FileInputStream(args[0]);
+			File sourceFile = new File(args[0]);
+			FileInputStream sourceStream = new FileInputStream(sourceFile);
 			ShadowParser parser = new ShadowParser(sourceStream);
+	        TypeChecker tc = new TypeChecker(false);
+	        TACBuilder tacBuilder = new TACBuilder(false);
+	        SimpleNode node = parser.CompilationUnit();
+	        
+	        long startTime = System.currentTimeMillis();
+
+	        // type check the tree
+	        boolean result = tc.typeCheck(node);
+	        
+	        if(!result) {
+	        	System.out.println(sourceFile.getPath() + "FAILED TO TYPE CHECK");
+	        	return;
+	        }
+
+	        // build the TAC
+	        tacBuilder.build(node);
+
+    		for(TACMethod m:tacBuilder.methods) {
+    			TACCVisitor cVisitor = new TACCVisitor(m.getEntry());
+    			TACLinearWalker linearWalker = new TACLinearWalker(cVisitor);
 			
-			// parse the translation unit
-			SimpleNode n = parser.CompilationUnit();
-			
-			n.dump("");
-			System.out.println("GOOD PARSE");
+    			linearWalker.walk();
+    		}
+    		
+	        long stopTime = System.currentTimeMillis();
+	        long runTime = stopTime - startTime;
+
+	        System.err.println("COMPILED " + args[0] + " in " + runTime + "ms");
 			
 		} catch(FileNotFoundException fnfe) {
 			System.err.println("Source file (" + args[0] + ") not found: " + fnfe.getLocalizedMessage());
 		} catch(ParseException pe) {
 			System.err.println("Error parsing " + args[0]);
 			System.err.println(pe.getLocalizedMessage());
+		} catch (ShadowException e) {
+			e.printStackTrace();
 		}
 	}
 
