6208c22f100a2778740b705c71e65707fd4bcdbd
wspeirs <wspeirs@d82a048f-d6e0-442c-bf07-03e2631dfcd4>
2011-03-24 01:59:11 +0000

    - Removed parser test

--- src/compiler/src/shadow/test/AllTests.java
+++ src/compiler/src/shadow/test/AllTests.java
@@ -2,10 +2,9 @@ package shadow.test;
 
 import java.io.IOException;
 
-import shadow.parser.javacc.ShadowException;
-import shadow.parser.test.ParserTest;
-import shadow.typecheck.test.TypeCheckerTest;
 import shadow.TAC.test.TACTest;
+import shadow.parser.javacc.ShadowException;
+import shadow.typecheck.test.TypeCheckerTest;
 
 public class AllTests {
 
@@ -14,11 +13,9 @@ public class AllTests {
 	 */
 	public static void main(String[] args) throws IOException, ShadowException {
 		// add any new tests here
-		ParserTest pt = new ParserTest(false, true);	// dump = false, debug = true
 		TypeCheckerTest tct = new TypeCheckerTest(false, true); // dump = false, debug = true
 		TACTest tact = new TACTest(true, true); // dump = false, debug = true
 
-		pt.testAll();
 		tct.testAll();
 		tact.testAll();
 	}
