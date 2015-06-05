8e0068de01d9415c280034758c2b94f9eed120d8
Brian (Brinsky) <Brinsky@users.noreply.github.com>
2015-06-05 11:34:15 -0400

    Added a test regarding misplaced documentation comments

---
+++ src/test/java/shadow/test/doctool/DocumentationTest.java
@@ -0,0 +1,28 @@
+package shadow.test.doctool;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import shadow.doctool.DocumentationTool;
+
+public class DocumentationTest 
+{
+	private ArrayList<String> args = new ArrayList<String>();
+	
+	@Before
+	public void setup() throws Exception 
+	{
+		if( System.getProperty("os.name").contains("Windows") ) {
+			args.add("-c");
+			args.add("windows.xml");
+		}
+	}
+	
+	@Test public void misplacedTest() throws Exception 
+	{
+		args.add("shadow/test/doctool/Misplaced.shadow");
+		DocumentationTool.main(args.toArray(new String[] { }));
+	}	
+}
